afee11ce01a526a815b100a0df4153b5e36979d7
Milan Skocic <milan.skocic@gmail.com>
2021-09-15 00:07:56 +0200

    DOC: Brief description of translation unit.

--- src/io.c
+++ src/io.c
@@ -1,5 +1,6 @@
 /**
  * @file io.c
+ * @brief IO funtions.
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,9 +16,10 @@
  * @param[out] nrows Number of rows in the file
  * @param[out] ncols Number of columns in the file
  */
-static int io_nrow_ncol(FILE *stream, const size_t buffer_size, const size_t skip_header, 
-                char *separators, 
-                size_t *nrows, size_t *ncols){
+static int io_nrow_ncol(FILE *stream, const size_t buffer_size, const size_t skip_header,
+                        char *separators,
+                        size_t *nrows, size_t *ncols)
+{
 
     size_t i, j, k;
     int status;
@@ -34,19 +36,23 @@ static int io_nrow_ncol(FILE *stream, const size_t buffer_size, const size_t ski
     errno = 0;
 
     // count number of rows
-    while(!feof(stream)&(!errno)){
+    while (!feof(stream) & (!errno))
+    {
         // firts line count nubmber of cols
         strcpy(line, "");
         fgets(line, buffer_size, stream);
-        if(k>=skip_header){
+        if (k >= skip_header)
+        {
             *ncols = j;
             j = 0;
             token = strtok(line, separators);
-            while(token != NULL){
+            while (token != NULL)
+            {
                 token = strtok(NULL, separators);
                 j++;
             }
-            if((j != (*ncols))&(k>skip_header)){
+            if ((j != (*ncols)) & (k > skip_header))
+            {
                 errno = EBADR;
             }
             token = NULL;
@@ -62,16 +68,14 @@ static int io_nrow_ncol(FILE *stream, const size_t buffer_size, const size_t ski
     return errno;
 }
 
-static int io_read_z_data(FILE *stream, size_t buffer_size, 
-                        size_t skip_header,
-                        size_t nrows, size_t ncols){
-    
+static int io_read_z_data(FILE *stream, size_t buffer_size,
+                          size_t skip_header,
+                          size_t nrows, size_t ncols)
+{
+
     errno = 0;
 
-    
-    
     return errno;
-
 }
 
 /**
@@ -79,7 +83,8 @@ static int io_read_z_data(FILE *stream, size_t buffer_size,
  * @param[in] fpath Path to the data file
  * @param[in] verbose Flag for verbose output 
  */
-int read_z(char *fpath, int verbose){
+int read_z(char *fpath, int verbose)
+{
 
     FILE *stream;
     double *data_z;
@@ -97,7 +102,8 @@ int read_z(char *fpath, int verbose){
 
     errno = io_nrow_ncol(stream, buffer_size, skip_header, separators, &nrows, &ncols);
 
-    if(verbose){
+    if (verbose)
+    {
         printf("N rows= %ld\n", nrows);
         printf("N cols= %ld\n", ncols);
         printf("Errno: %d - %s", errno, strerror(errno));
@@ -105,8 +111,7 @@ int read_z(char *fpath, int verbose){
 
     fclose(stream);
 
-    data_z = (double *)calloc(nrows*ncols, sizeof(double));
-
+    data_z = (double *)calloc(nrows * ncols, sizeof(double));
 
     free(data_z);
 

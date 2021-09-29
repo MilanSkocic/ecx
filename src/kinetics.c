8db771af082ef3e711d329c74294152c1dd7da61
Milan Skocic <milan.skocic@gmail.com>
2021-09-30 00:54:03 +0200

    ENH: Added examples for kinetics.

--- src/kinetics.c
+++ src/kinetics.c
@@ -6,7 +6,7 @@
  * @details Kinetics equations:
  *  - butler-volmer without mass transport
  *  - butler-volmer with mass transport
- *
+ * 
  * Copyright (C) 2020-2021  Milan Skocic.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -24,12 +24,42 @@
  *
  * Author: Milan Skocic <milan.skocic@gmail.com>
  *
+ * @example example_kinetics.c
+ * @details How to use bv()
  */
 
 #include "electrox.h"
 
 /**
- * @brief Compute Butler Volmer equation.
+ * @brief Compute Butler Volmer equation without mass transport.
+ * @details \f$ \eta' = \frac{\eta}{RT/F} \f$ 
+ * 
+ * \f$ j = j_0 ( \exp(\alpha_a z_a\eta') -\exp(-\alpha_c z_c\eta') )
+ * @param[in] OCV Open Circuit Potential in Volts
+ * @param[in] U Potential to compute in Volts
+ * @param[in] j0 Exchange current density in A.cm-2
+ * @param[in] aa Anodic transfer coefficient
+ * @param[in] ac Cathodic transfer coefficient
+ * @param[in] za Anodic number of exchanged electrons
+ * @param[in] zc Cathodic number of exchanged electrons
+ * @param[in] S Area in cm2
+ * @param[in] T Temperature in °C
+ * @return i Current in A.
+ */
+double sbv(double OCV, double U, double j0,
+           double aa, double ac, double za, double zc,
+           double S, double T)
+{
+    double kTe;
+    double i;
+
+    kTe = kB * (T + T_K) / qe;
+    i = j0 * (exp(aa * za * (U - OCV) / kTe) - exp(-ac * zc * (U - OCV) / kTe));
+
+    return i;
+}
+/**
+ * @brief Compute Butler Volmer equation with mass transport.
  * @details \f$ \eta' = \frac{\eta}{RT/F} \f$ 
  * 
  * \f$ j = j_0 \frac{\exp(\alpha_a z_a\eta') -\exp(-\alpha_c z_c\eta')}
@@ -57,14 +87,10 @@ double bv(double OCV, double U, double j0, double jdla, double jdlc,
     double i;
 
     kTe = kB * (T + T_K) / qe;
-    num = j0 * (exp(aa * za * (U - OCV) / kTe) - exp(-ac * zc * (U - OCV) / kTe));
+    num = sbv(OCV, U, j0, aa, ac, za, zc, S, T);
     denom = 1 + j0 / jdla * exp(aa * za * (U - OCV) / kTe) + j0 / jdlc * exp(-ac * zc * (U - OCV) / kTe);
 
     i = S * num / denom;
 
     return i;
 }
-/**
- * @example example1.c
- * @details Example of how to use bv()
- */

09cdc9a99d6d9bf31a80d003efb65722a46f39fa
Milan Skocic <milan.skocic@icloud.com>
2022-07-21 21:52:31 +0200

    ENH: Add config executable for the library.

--- src/version.c
+++
@@ -1,8 +0,0 @@
- #include "electrox.h"
- #include "config.h"
-
- int main(int argc, char **argv){
-
-     printf("%s v.%s in C.\n%s\n", PROJECT_NAME, PROJECT_VERSION, PROJECT_DESCRIPTION, PROJECT_LANGUAGES);
-     EXIT_SUCCESS;
- }

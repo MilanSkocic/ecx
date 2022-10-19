35ad285b8dc92f3af5f96b77e7a3a89a6b1359e0
Milan Skocic <milan.skocic@gmail.com>
2022-10-19 12:06:39 +0200

    DEV: Add fortran version of resistance.

--- src/eis.f90
+++ src/eis.f90
@@ -4,17 +4,24 @@
 
 
 !!
-!> @brief Compute resistance impedance in vectorized form. 
-!! @details \f$ Z = p_0 \forall \omega \f$
-!! @param[in] p Pointer to the resistance in Ohms.
-!! @param[in] w Pointer to th angular frequencies in rad.s^-1.
-!! @param[out] Z Pointer to the complex impedance in Ohms.
-subroutine resistance(Z, N, R)
+!> @brief Compute resistance impedance. 
+!! @details \f$ Z = R \forall \omega \f$
+!! @param[in] w Angular frequencies in rad.s^-1.
+!! @param[in] R Resistance in Ohms.
+!! @param[in] N Size of the impedance array.
+!! @param[out] Z Complex impedance array in Ohms.
+subroutine impedance_resistance(w, R, N, Z)
     use iso_c_binding
-    integer(c_size_t), intent(in) :: N
-    real(8), intent(in) :: R
-    real(8), intent(out), dimension(N) :: Z
 
-    Z(:) = R
+    integer(c_size_t), intent(in) :: N
+    real(c_double), intent(in) :: R
+    real(c_double), intent(in), dimension(N) :: w
+    complex(c_double_complex), intent(out), dimension(N) :: Z
+
+    integer(c_size_t) :: i
+
+    do i=1, N
+        Z(i) = complex(R, 0.0)
+    enddo
 
 end subroutine

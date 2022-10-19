95ddba28f0721a5aa835c93366035affd4d8fef3
Milan Skocic <milan.skocic@gmail.com>
2022-10-19 13:05:44 +0200

    ENH: Remove useless extern keyword.

--- src/eis.h
+++ src/eis.h
@@ -1,8 +1,16 @@
+/**
+ * @file eis.h
+ * 
+ * @brief Declaration of Fortran functions.
+ * 
+ * @details External link to the fortran functions used in the C wrappers.
+ * 
+ */
 #ifndef EIS_H
 #define EIS_H
 #include <stdlib.h>
 #include <complex.h>
 
-extern void impendance_resistance_(double *w, double *R, size_t *N, double complex *Z);
+void impedance_resistance_(double *w, double *R, size_t *N, double complex *Z);
 
 #endif

70bcfa6b607efa8dfd7f020cbfd4ac730490db0f
Milan Skocic <milan.skocic@icloud.com>
2023-03-13 08:08:12 +0100

    ENH: Migration to fortran.

--- src/ecx.f90
+++ src/ecx.f90
@@ -1,22 +1,21 @@
-!>
-!> @file eis.f90
-!!
+!> @file
+!! Electrochemistry module.
 
-!> @brief Module for Electrochemical Impedance Spectroscopy
-module eis
+!> @brief Electrochemistry module.
+module ecx
     use iso_fortran_env
-    use iso_c_binding
     implicit none
+    private
 
+public :: ecx_z_r, ecx_z_c
 
 contains
 
-!!
 !> @brief Compute resistance impedance. 
 !! @details \f$ Z = R \forall \omega \f$
 !! @param[in] w Angular frequencies in rad.s^-1.
 !! @param[in] R Resistance in Ohms.
-pure elemental function resistance_impedance(w, R) result(Z)
+pure elemental function ecx_z_r(w, R) result(Z)
     implicit none
 
     real(real64), intent(in) :: R
@@ -31,7 +30,7 @@ end function
 !! @details \f$ Z = 1/(jCw) \f$
 !! @param[in] w Angular frequencies in rad.s^-1.
 !! @param[in] R Resistance in Ohms.
-pure elemental function capacitance_impedance(w, C) result(Z)
+pure elemental function ecx_z_c(w, C) result(Z)
     implicit none
 
     real(real64), intent(in) :: C
@@ -40,5 +39,4 @@ pure elemental function capacitance_impedance(w, C) result(Z)
 
     Z = cmplx(0.0d0, -1/(C*w), kind=real64)
 end function
-
 end module
---
+++ src/ecx_capi.f90
@@ -0,0 +1,29 @@
+module ecx_capi
+    use iso_fortran_env
+    use iso_c_binding
+    use ecx
+    implicit none
+    private
+
+contains
+
+
+!> @brief Compute resistance impedance. 
+!! @details \f$ Z = R \forall \omega \f$
+!! @param[in] w Angular frequencies in rad.s^-1 (rank-1 array)
+!! @param[in] R Resistance in Ohms.
+!! @param[in] n Size of w and Z.
+!! @param[out] Z Complex impedance in Ohms (rank-1 array).
+
+pure subroutine ecx_capi_z_r(w, R, n, Z)bind(C)
+    implicit none
+
+    integer(c_size_t), intent(in) :: n
+    real(c_double), intent(in) :: R
+    real(c_double), dimension(n), intent(in) :: w
+    complex(c_double_complex), dimension(n), intent(out) :: Z
+
+    Z(:) = ecx_z_r(w, r)
+end subroutine
+
+end module

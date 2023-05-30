0046df7a4d5492cd12fac64f07d65dda890ba2aa
Milan Skocic <milan.skocic@icloud.com>
2023-05-30 07:46:43 +0200

    DOC: Update documentation.

--- documentation/sphinx/source/conf.py
+++ documentation/sphinx/source/conf.py
@@ -89,7 +89,7 @@ ford_code_gray = "#f5f5f5"
 ford_white = "#FFFFFF"
 ford_black = "#000000"
 ford_purple = "#734f96"
-html_theme_options = {"nosidebar": False,
+html_theme_options = {"nosidebar": True,
                       "rightsidebar": False,
                       "headfont": "Helvetica Neue",
                       "bodyfont": "Helvetica Neue",

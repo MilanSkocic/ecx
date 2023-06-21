bca8462ea42091677927075b8993337979b484b2
Milan Skocic <milan.skocic@icloud.com>
2023-06-21 06:35:47 +0200

    ENH: Switch to shared lib for pywrapper.

--- pywrapper/setup.py
+++ pywrapper/setup.py
@@ -1,5 +1,6 @@
 import importlib
 import pathlib
+import platform
 from setuptools import setup, find_packages, Extension
 
 # Import only version.py file for extracting the version
@@ -7,13 +8,32 @@ spec = importlib.util.spec_from_file_location('version', './pyecx/version.py')
 mod = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(mod)
 
+libraries = None
+library_dirs = None
+runtime_library_dirs = None
+extra_objects = None
+
+if platform.system() == "Linux":
+    libraries = ["ecx"]
+    library_dirs = ["./pyecx"]
+    runtime_library_dirs = ["$ORIGIN"]
+if platform.system() == "Windows":
+    extra_objects = ["./pyecx/libecx.dll.a"]
+if platform.system() == "Darwin":
+    libraries = ["ecx"]
+    library_dirs = ["./pyecx"]
+    runtime_library_dirs = ["@loader_path"]
+
 
 if __name__ == "__main__":
 
     mod_ext = Extension(name="pyecx.eis",
                         sources=["./pyecx/ecx_eis.c"],
-                        extra_objects=["./pyecx/libecx.a"])
-    
+                        libraries=libraries,
+                        library_dirs=library_dirs,
+                        runtime_library_dirs=runtime_library_dirs,
+                        extra_objects=extra_objects)
+
     setup(name=mod.__package_name__,
         version=mod.__version__,
         maintainer=mod.__maintainer__,
@@ -25,10 +45,11 @@ if __name__ == "__main__":
         url='https://milanskocic.github.io/ecx/index.html',
         download_url='https://github.com/MilanSkocic/pyecx',
         packages=find_packages(),
-        include_package_data=True,
-        python_requires='>=3.7',
+        include_package_data=False,
+        package_data={"pyecx": ["libecx.*"]},
+        python_requires='>=3.8',
         install_requires=pathlib.Path("requirements.txt").read_text(encoding="utf-8").split('\n'),
-        classifiers=["Development Status :: 5 - Stable",
+        classifiers=["Development Status :: 5 - Production/Stable",
                     "Intended Audience :: Science/Research",
                     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"],
         ext_modules=[mod_ext]

203fab8407acb4fd90a218c1d88d20d1763ba17c
Milan Skocic <milan.skocic@icloud.com>
2023-09-06 07:51:38 +0200

    ENH: C API for bv and nernst in kinetics module.

--- include/ecx_kinetics.h
+++ include/ecx_kinetics.h
@@ -8,7 +8,16 @@
 #include "ecx_types.h"
 #include "ecx_core.h"
 
+extern double ecx_kinetics_capi_nernst(double E0, size_t z, 
+                                       double *aox, double *vox, size_t nox, 
+                                       double *ared, double *vred, size_t nred, 
+                                       double T);
+
 extern void ecx_kinetics_capi_sbv(double *U, double OCV, double j0, 
+                        double aa, double ac, double za, double zc,
+                        double A, double T, double *i, size_t n);
+
+extern void ecx_kinetics_capi_bv(double *U, double OCV, double j0, double jdla, double jdlc,
                         double aa, double ac, double za, double zc,
                         double A, double T, double *i, size_t n);
 
--- src/ecx_kinetics_capi.f90
+++ src/ecx_kinetics_capi.f90
@@ -7,6 +7,32 @@ module ecx__kinetics_capi
 
 contains
 
+pure function ecx_kinetics_capi_nernst(E0, z, aox, vox, nox, ared, vred, nred, T)result(E)bind(C)
+    !! Compute the Nernst electrochemical potential in V.
+    implicit none
+    real(c_double), intent(in), value :: E0
+        !! Standard electrochemical potential in V.
+    real(c_double), intent(in), value :: z
+        !! Number of exchanged electrons.
+    integer(c_size_t), intent(in), value :: nox
+        !! Number of oxidants.
+    integer(c_size_t), intent(in), value :: nred
+        !! Number of reductants.
+    real(c_double), intent(in) :: aox(nox)
+        !! Activities of the oxidants.
+    real(c_double), intent(in) :: vox(nox)
+        !! Coefficients for the oxidants.
+    real(c_double), intent(in) :: ared(nred)
+        !! Activities of the reductants
+    real(c_double), intent(in) :: vred(nred)
+        !! Coefficients for the reductants.
+    real(c_double), intent(in), value :: T
+        !! Temperature in °C.
+    real(c_double) :: E
+    E = ecx_kinetics_nernst(E0, z, aox, vox, ared, vred, T)
+
+end function
+
 pure subroutine ecx_kinetics_capi_sbv(U, OCV, j0, aa, ac, za, zc, A, T, I, n)bind(c)
     !! Compute Butler Volmer equation without mass transport.
     ! arguments
@@ -48,9 +74,9 @@ pure subroutine ecx_kinetics_capi_bv(U, OCV, j0, jdla, jdlc, aa, ac, za, zc, A, 
         !! Potential in volts.
     real(c_double), intent(in), value :: j0
         !! Exchange current density in A.cm-2
-    real(real64), intent(in) :: jdla
+    real(real64), intent(in), value :: jdla
         !! Anodic diffusion limiting current density in A.cm-2.
-    real(real64), intent(in) :: jdlc
+    real(real64), intent(in), value :: jdlc
         !! Cathodic diffusion limiting current density in A.cm-2.
     real(c_double), intent(in), value :: aa
         !! Anodic transfert coefficient.

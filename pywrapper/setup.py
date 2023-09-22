c5286b9c8cdc2e7fd2c44813f7ecfbdea6a4faa9
Milan Skocic <milan.skocic@icloud.com>
2023-09-22 06:59:11 +0200

    BLD: Switch pyproject.toml

--- pywrapper/setup.py
+++ pywrapper/setup.py
@@ -3,11 +3,6 @@ import pathlib
 import platform
 from setuptools import setup, find_packages, Extension
 
-# Import only version.py file for extracting the version
-spec = importlib.util.spec_from_file_location('version', './pyecx/version.py')
-mod = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(mod)
-
 libraries = None
 library_dirs = None
 runtime_library_dirs = None
@@ -41,29 +36,4 @@ if __name__ == "__main__":
                         runtime_library_dirs=runtime_library_dirs,
                         extra_objects=extra_objects)
 
-    setup(name=mod.__package_name__,
-        version=mod.__version__,
-        maintainer=mod.__maintainer__,
-        maintainer_email=mod.__maintainer_email__,
-        author=mod.__author__,
-        author_email=mod.__author_email__,
-        description=mod.__package_name__,
-        long_description=pathlib.Path("README.rst").read_text(encoding="utf-8"),
-        url='https://milanskocic.github.io/ecx/index.html',
-        download_url='https://github.com/MilanSkocic/pyecx',
-        packages=find_packages(),
-        include_package_data=False,
-        package_data={"pyecx": ["libecx.*"]},
-        python_requires='>=3.8',
-        install_requires=pathlib.Path("requirements.txt").read_text(encoding="utf-8").split('\n'),
-        classifiers=["Development Status :: 5 - Production/Stable",
-                    "Intended Audience :: Science/Research",
-                    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"],
-        ext_modules=[mod_core, mod_eis]
-        )
-
-
-# pypi
-# >>> python setup.py sdist bdist_wheel
-# >>> python -m twine upload dist/*
-# >>> python -m twine upload --repository testpypi dist/*
+    setup(ext_modules=[mod_core, mod_eis])

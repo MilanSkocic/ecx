87a8f3d3ba1d67849ac2da54021f07746fe7f7c7
Milan Skocic <milan.skocic@icloud.com>
2023-12-07 13:29:31 +0100

    BLD: Fix minor issues after testing on windows.

--- include/ecx_core.h
+++ include/ecx_core.h
@@ -5,10 +5,15 @@
  */
 #ifndef ECX_CORE_H
 #define ECX_CORE_H
+#if _MSC_VER
+#define ADD_IMPORT __declspec(dllimport)
+#else
+#define ADD_IMPORT
+#endif
 #include "ecx_types.h"
 
-extern const double ecx_core_capi_PI; /**< PI */
-extern const double ecx_core_capi_T_K; /**< 0°C in Kelvin */
+ADD_IMPORT const double ecx_core_capi_PI; /**< PI */
+ADD_IMPORT extern const double ecx_core_capi_T_K; /**< 0°C in Kelvin */
 void ecx_core_capi_nm2eV(double *lambda, double *E, size_t n); /**< Convert wavelength (nm) to energy (eV)*/
 void ecx_core_capi_kTe(double *U, double *kTE, size_t n); /**< Compute the thermal voltage (V) from temperature (°C)*/
 

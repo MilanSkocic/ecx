b97cbe9ae1186eddfaf60ddbe0095e6fcfb47823
Milan Skocic <milan.skocic@icloud.com>
2024-04-01 07:25:11 +0200

    DOC: Add doc to eis C header.

--- include/ecx_eis.h
+++ include/ecx_eis.h
@@ -1,6 +1,6 @@
 /**
  * @file 
- * @brief EIS C header for th ecx library.
+ * @brief EIS C header for the ecx library.
  * @details Complex impedance
  */
 #ifndef ECX_EIS_CAPI_H
@@ -13,6 +13,17 @@ extern void ecx_capi_zc(double *w, double C, size_t n, ecx_cdouble *Z);
 extern void ecx_capi_zl(double *w, double L, size_t n, ecx_cdouble *Z);
 extern void ecx_capi_zcpe(double *w, double Q, double a, size_t n, ecx_cdouble *Z);
 extern void ecx_capi_zw(double *w, double s, size_t n, ecx_cdouble *Z);
-extern void ecx_eis_z(double *p, double *w, ecx_cdouble *z, char e, size_t k, size_t n, int *errstat);
+
+/**
+ * @brief C API Compute the complex impedance for the given element.
+ * @param[in] n Size of w
+ * @param[in] k Size of p
+ * @param[in] e Electrochemical element: R, C, L, Q, O, T, G
+ * @param[in] errstat Error status
+ * @param[in] p Parameters.
+ * @param[in] w Angular frequencies in rad.s-1
+ * @param[in] z Complex impedance in Ohms.
+ */
+extern void ecx_eis_z(double *p, double *w, ecx_cdouble *z, char e, size_t k, size_t n, int *errstat); 
 
 #endif

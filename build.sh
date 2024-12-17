643b6d36cc1bc98f64f3487f15deac473dd87f14
Milan Skocic <milan.skocic@icloud.com>
2024-12-17 17:39:12 +0100

    Update build for darwin rpaths.

--- build.sh
+++ build.sh
@@ -18,9 +18,9 @@ done
 
 echo ""
 echo "DEPLOY LIBS TO PYTHON"
-cp -vf $d/bin/* py/src/$PYNAME/
-cp -vf $d/include/$NAME*.h py/src/$PYNAME/
-cp -vf $d/lib/* py/src/$PYNAME/
+cp -vf $d/bin/* $PY_SRC/
+cp -vf $d/include/$NAME*.h $PY_SRC/
+cp -vf $d/lib/* $PY_SRC/
 
 echo ""
 echo "ZIP"
@@ -28,4 +28,14 @@ cd $d/
 zip -r $NAME-$PLATFORM-$ARCH.zip .
 cd ../../
 mv $d/$NAME-$PLATFORM-$ARCH.zip ./build/
+
+if [[ $PLATFORM == "darwin" ]]; then
+    echo ""
+    echo "CHECK RPATHS FOR DARWIN"
+    for lib in ${LIBS[@]}; do
+        otool -L $d/lib/$lib$EXT
+        otool -L $PY_SRC/$lib$EXT
+done
+
+fi
 

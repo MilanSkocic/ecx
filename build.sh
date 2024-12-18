7a050761a685e1c2fbd0f68dd3e4068016dce8eb
Milan Skocic <milan.skocic@icloud.com>
2024-12-18 07:49:21 +0100

    Comment out section for darwin with shared libraries.

--- build.sh
+++ build.sh
@@ -16,7 +16,6 @@ make install prefix=$d
 #     fi
 # done
 
-
 echo ""
 echo "DEPLOY LIBS TO PYTHON"
 cp -vf $d/bin/* py/$PY_SRC/
@@ -24,21 +23,22 @@ cp -vf $d/include/$NAME*.h py/$PY_SRC/
 cp -vf $d/lib/* py/$PY_SRC/
 
 
-if [[ $PLATFORM == "darwin" ]]; then
-    echo ""
-    echo "CHECK RPATHS FOR DARWIN"
-    # for lib in ${LIBS[@]}; do
-    #     install_name_tool -change /usr/local/opt/gcc@10/lib/gcc/10/$lib$EXT @loader_path/$lib$EXT $d/lib/$LIBNAME$EXT
-    #     install_name_tool -change /usr/local/opt/gcc@10/lib/gcc/10/$lib$EXT @loader_path/$lib$EXT py/$PY_SRC/$LIBNAME$EXT
-    # done
-    otool -L $d/lib/$LIBNAME$EXT
-fi
+# if [[ $PLATFORM == "darwin" ]]; then
+#     echo ""
+#     echo "CHECK RPATHS FOR DARWIN"
+#     # in case od shared libs: change rpaths for dependencies
+#     for lib in ${LIBS[@]}; do
+#         install_name_tool -change /usr/local/opt/gcc@10/lib/gcc/10/$lib$EXT @loader_path/$lib$EXT $d/lib/$LIBNAME$EXT
+#         install_name_tool -change /usr/local/opt/gcc@10/lib/gcc/10/$lib$EXT @loader_path/$lib$EXT py/$PY_SRC/$LIBNAME$EXT
+#     done
+#     otool -L $d/lib/$LIBNAME$EXT
+# fi
 
 echo ""
 echo "ZIP"
 cd $d/
-zip -r $NAME-$PLATFORM-$ARCH.zip .
+zip -r $NAME-$VERSION-$PLATFORM-$ARCH.zip .
 cd ../../
-mv $d/$NAME-$PLATFORM-$ARCH.zip ./build/
+mv $d/$NAME-$VERSION-$PLATFORM-$ARCH.zip ./build/
 
 

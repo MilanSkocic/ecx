7a7c806202aa97b4111ebd89d5db8ec43a5ce888
Milan Skocic <milan.skocic@gmail.com>
2026-01-06 06:57:32 +0100

    Improve CLI interface for saw subcommand.

--- app/main.f90
+++ app/main.f90
@@ -9,10 +9,11 @@ program ecxcli
     integer :: i
     real(real64) :: r, dr
     character(len=32) :: cmd
-    character(len=:),allocatable  :: help_text(:), version_text(:)
-    logical :: flag_abridged
+    character(len=:),allocatable, target  :: help_text(:), version_text(:), usage_text(:)
+    character(len=:), pointer :: char_fp(:)
     character(len=3) :: elmt
-
+    
+    nullify(char_fp)
 
     help_text=[character(len=80) :: &
         'NAME                                                            ', &
@@ -37,8 +38,12 @@ program ecxcli
         !'  o ice  Get the isotopic composition of the element.           ', &
         !'  o naw  Get the nuclide atomic weight.                         ', &
         '                                                                ', &
+        '  Enter ecxcli SUBCOMMAND --help for detailed descriptions.     ', &
+        '                                                                ', &
         'OPTIONS                                                         ', &
-        '  o --not-abridged  Do not use the abridged value.              ', &
+        '  o --abridged, -a     Use the abridged value.                  ', &
+        '  o --uncertainty, -u  Use the uncertainty.                     ', &
+        '  o --pprint           Nice formatting.                         ', &
         '                                                                ', &
         'VALID FOR ALL SUBCOMMANDS                                       ', &
         '  o --help     Show help text and exit                          ', & 
@@ -55,34 +60,46 @@ program ecxcli
         'LICENSE:      MIT                   ', &
         '' ]
 
+    usage_text=[character(len=80) :: &
+        'Usage: ecxcli SUBCOMMAND [OPTIONS...]|[--help|--version] ELEMENTS...', &
+        '' ]
 
     cmd = get_subcommand()
     call set_mode('strict')
 
     select case (cmd)
         case ("saw")
-            call set_args("--abridged:a", help_saw(), version_text)
-            flag_abridged = lget('a')
+            call set_args("--abridged:a --uncertainty:u --pprint", get_help_saw(), version_text)
+            if(size(unnamed) == 1) then
+                write(output_unit, "(A)") "Enter at least one element."
+                char_fp => usage_text
+                call print_text(char_fp)
+                stop
+            end if
             do i=2, size(unnamed)
                 elmt = unnamed(i)
-                r = get_saw(elmt,abridged=flag_abridged)
-                dr = get_saw(elmt,abridged=flag_abridged, uncertainty=.true.)
-                write(output_unit, '(A4, A3, A3, SP, G14.6, A3, ES14.2)') &
+                if(lget("pprint"))then
+                    r = get_saw(elmt,abridged=lget("a"))
+                    dr = get_saw(elmt, abridged=lget("a"), uncertainty=.true.)
+                    write(output_unit, '(A4, A3, A3, SP, G14.6, A3, ES14.2)') &
                      'SAW_', elmt, " = ", r, "+/-", dr
+                else
+                    r = get_saw(elmt,abridged=lget("a"), uncertainty=lget("u"))
+                    write(output_unit, '(G0.16)') r
+                end if
             end do
         case ("all")
             call set_args(" ", help_text, version_text)
             call  print_periodic_table()
         case default
             call set_args(" ", help_text, version_text)
-            do i=1, size(help_text), 1
-                write (OUTPUT_UNIT, '(A)') help_text(i)
-            end do
+            char_fp => usage_text
+            call print_text(char_fp)
     end select
 
 contains
 
-function help_saw()result(res)
+function get_help_saw()result(res)
     !! Get the help text for the subcommand saw.
     character(len=80), allocatable :: res(:)
     res = [character(len=80) :: &
@@ -90,22 +107,39 @@ function help_saw()result(res)
         '  saw - th ecxcli subcommand to get the standard atomic weight. ', &
         '                                                                ', &
         'SYNOPSIS                                                        ', &
-        '  ecxcli saw [--not-abridged] ELEMENTS ...                      ', &
+        '  ecxcli saw [OPTIONS ...] ELEMENTS ...                      ', &
         '                                                                ', &
         'DESCRIPTION                                                     ', &
         '  Provide the saw either abridged or not.                       ', &
         '  The uncertainty of the saw can be retrieved too.              ', &
         '                                                                ', &
         'OPTIONS                                                         ', &
-        '  o --not-abridged  Do not use the abridged value.              ', &
+        '  o --abridged, -a     Use the abridged value.                  ', &
+        '  o --uncertainty, -u  Use the uncertainty.                     ', &
+        '  o --pprint           Nice formatting.                         ', &
         '                                                                ', &
         'VALID FOR ALL SUBCOMMANDS                                       ', &
         '  o --help     Show help text and exit                          ', & 
         '  o --verbose  Display additional information when available.   ', &
         '  o --version  Show version information and exit.               ', &
         '                                                                ', &
+        'EXAMPLE                                                         ', &
+        '  Minimal example                                               ', &
+        '      ecxcli saw H                                              ', &
+        '      ecxcli saw -a -u O                                        ', &
+        '      ecxcli saw H C B O Zr Nb --pprint                         ', &
+        '      ecxcli saw -a H C B O Zr Nb --pprint                      ', &
+        '                                                                ', &
         '' ]
-
 end function
+
+subroutine print_text(char_fp)
+    character(len=:), pointer, intent(in) :: char_fp(:)
+    integer :: i
+    do i=1, size(char_fp), 1
+        write (OUTPUT_UNIT, '(A)') char_fp(i)
+    end do
+end subroutine
+
 
 end program

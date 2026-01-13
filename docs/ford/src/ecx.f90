3719e0cd4269666143dd8bcb81aae2b1e2abc244
Milan Skocic <milan.skocic@gmail.com>
2026-01-13 07:04:24 +0100

    Update online documentation.

--- docs/ford/src/ecx.f90
+++
@@ -1,5 +0,0 @@
-module ecx
-    !! Main module for ecx.
-    use ecx__api
-    use ecx__capi
-end module
--- docs/ford/src/ecx_version.f90
+++
@@ -1,6 +0,0 @@
-module ecx__version
-    !! Version
-    implicit none
-    private
-    character(len=*), parameter, public :: version = "0.1.0dev0"
-end module ecx__version
